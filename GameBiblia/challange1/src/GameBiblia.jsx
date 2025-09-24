4f277a1fdf159cf41ad8b94d4c810bf9cb097d01
UlissesNunes <ulissesnunesx@gmail.com>
2025-09-23 22:40:00 -0300

    Organizando projeto

---
+++ GameBiblia/challange1/src/GameBiblia.jsx
@@ -0,0 +1,22 @@
+
+
+import './GameBibliaGlobal.css'
+
+function GameBiblia() {
+
+
+ 
+ 
+  return (
+    <>
+   <div className="flex justify-center items-center h-screen">
+  <p>Olá, Ulisses!</p>
+</div>
+
+   
+
+    </>
+  )
+}
+
+export default GameBiblia
