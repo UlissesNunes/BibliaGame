f1d454b7c21071a39f0ec9e9ef14aedb2910cd7e
UlissesNunes <ulissesnunesx@gmail.com>
2025-10-03 22:01:09 -0300

    ok

--- GameBiblia/challange1/src/pages/StartScreean.jsx
+++ GameBiblia/challange1/src/pages/StartScreean.jsx
@@ -5,10 +5,10 @@ function StartScreean({StartGameOn}) {
 <>
   <main className="flex flex-col  items-center justify-start h-screen 
   bg-gradient-to-b from-green-50  to-green-100 xl:align-middle xl:items-center">
-    <h1 className="pt-6 pl-2 pr-2 text-center text-sm font-bold text-green-950  sm:text-lg md:text-lg md:w-4/5 lg:text-2xl xl:px-14 xl:py-10 xl:align-middle xl:w-2/3  2xl:text-5xl 2xl:pt-20">
+    <h1 className="pt-6 pl-2 pr-2 text-center text-sm font-bold text-green-950  sm:text-lg md:text-lg md:w-4/5 lg:text-2xl xl:px-14 xl:py-10 xl:align-middle xl:w-2/3  2xl:text-3xl 2xl:pt-20">
       DESAFIO: Garanta a maior pontuação, são perguntas bíblicas onde você deverá preencher o quadro com letras para formar a palavra de acordo com a dica fornecida.
     </h1>
-    <button onClick={StartGameOn} className="mt-8 w-32 bg-green-800 text-green-50 hover:text-green-100 py-2 px-4 rounded-md cursor-pointer hover:bg-green-900 transition duration-300 font-bold md:w-48 md:py-3 md:px-6 md:text-lg 2xl:px-4 2xl:py-5 2xl:w-80 2xl:text-4xl 2xl:m-14">
+    <button onClick={StartGameOn} className="mt-8 w-32 bg-green-800 text-green-50 hover:text-green-100 py-2 px-4 rounded-md cursor-pointer hover:bg-green-900 transition duration-300 font-bold md:w-48 md:py-3 md:px-6 md:text-lg 2xl:px-4 2xl:py-5 2xl:w-80 2xl:text-2xl 2xl:m-14">
       Iniciar jogo
     </button>
 
