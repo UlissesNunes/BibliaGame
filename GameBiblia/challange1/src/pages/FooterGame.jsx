a91d72465261057037523e01818015851de6b2ec
UlissesNunes <ulissesnunesx@gmail.com>
2025-10-07 11:43:03 -0300

    ok

--- GameBiblia/challange1/src/pages/FooterGame.jsx
+++ GameBiblia/challange1/src/pages/FooterGame.jsx
@@ -5,7 +5,7 @@ function FooterGame() {
     <><p className="flex justify-center  text-green-50 bg-gradient-to-b from-green-600 to-green-900 text-sm p-3 font-bold text-center " >@ 2025 Copy - Todos os direitos reservados</p><hr />
     <footer className="flex justify-center items-center p-2 bg-green-900">
 
-     <img src="src/assets/LOGO-BG (1).png" alt="img da logo do projeto" className="w-9 flex justify-center " />
+     <img src="/src/assets/logo-bg-1.png" alt="img da logo do projeto" className="w-9 flex justify-center " />
    
     </footer>
     </>
--- GameBiblia/challange1/src/pages/HeaderGame.jsx
+++ GameBiblia/challange1/src/pages/HeaderGame.jsx
@@ -6,7 +6,7 @@ function HeaderGame() {
     <header className="flex justify-start items-center p-4 bg-gradient-to-b from-green-800
     
     to-green-900 border-b-4 border-b-green-600">
-    <img src="assets/LOGO-BG (1).png" alt="img da logo do projeto" className="w-10 flex justify-start" />
+    <img src="/src/assets/logo-bg-1.png.png" alt="img da logo do projeto" className="w-10 flex justify-start" />
 
     <h1 className="w-full p-1 flex justify-center items-center text-center text-2xl text-green-100 font-bold">Você conhece a Biblia?</h1>
     
