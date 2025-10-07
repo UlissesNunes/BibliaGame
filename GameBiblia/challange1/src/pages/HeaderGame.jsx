a969fdc43c8f9ac74f2b14c7b0e5d1c65d48f02c
UlissesNunes <ulissesnunesx@gmail.com>
2025-10-07 11:33:01 -0300

    OK

--- GameBiblia/challange1/src/pages/HeaderGame.jsx
+++ GameBiblia/challange1/src/pages/HeaderGame.jsx
@@ -6,7 +6,7 @@ function HeaderGame() {
     <header className="flex justify-start items-center p-4 bg-gradient-to-b from-green-800
     
     to-green-900 border-b-4 border-b-green-600">
-    <img src="src/assets/LOGO-BG (1).png" alt="img da logo do projeto" className="w-10 flex justify-start" />
+    <img src="assets/LOGO-BG (1).png" alt="img da logo do projeto" className="w-10 flex justify-start" />
 
     <h1 className="w-full p-1 flex justify-center items-center text-center text-2xl text-green-100 font-bold">Você conhece a Biblia?</h1>
     
